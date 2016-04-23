5a4c90baa640f9ce19d9eca5d8895031470cb1cb
Artit Kiuwilai <first087@gmail.com>
2016-04-23 15:43:15 +0700

    Add workshop14

--- workshop6+/myservice.js
+++ workshop6+/myservice.js
@@ -49,6 +49,70 @@ app.use(bodyParser.json());
 //     });
 // });
 
+// Workshop 14 - Routing Center
+function execGet(req, res, next) {
+    var params = req.params[0].split('/');
+    console.log('params', params);
+    //[0]- empty,[1]-module,[2]-object,[3]-function,[4-n] params..n
+    if (params.length < 4)
+        res.send({code:530,status:"error",message:"Invalid parameters!"});
+    else {
+        var obj = require('api/'+params[1]+'/' +params[2]);
+        obj = new obj(app);
+        var func = obj[params[3]];
+        var inputs = [];
+        for (var i=4; i<params.length; i++) {
+            if (params[i])
+                inputs.push(params[i]);
+        }
+
+        console.log('inputs:', inputs);
+        func(inputs, function(result) {
+            res.send(result);
+        });
+    }
+}
+
+function execPost(req, res, next) {
+    var params = req.params[0].split('/');
+    console.log('params', params);
+    //[0]- empty,[1]-module,[2]-object,[3]-function,[4-n] params..n
+    if (params.length < 4)
+        res.send({code:530,status:"error",message:"Invalid parameters!"});
+    else {
+        try {
+            var obj = require('api/' + params[1] + '/' + params[2]);
+            obj = new obj(app);
+            var func = obj[params[3]];
+            func(req.body, function(result) {
+                console.log('result', result);
+                res.send(result);
+            });
+        } catch (err) {
+            res.send({code:500,status:"error",message:JSON.stringify(err)});
+        }
+    }
+}
+
+function executeApi(req, res, next) {
+    switch (req.method) {
+        case "GET":
+            execGet(req, res, next);
+        break;
+        case "POST":
+            execPost(req, res, next);
+        break;
+        case "PUT":
+            execPost(req, res, next);
+        break;
+        case "DELETE":
+            execPost(req, res, next);
+        break;
+    }
+}
+
+app.all('*', executeApi);
+
 app.listen(3000);
 console.log("My Service is listening to port 3000.");
 
